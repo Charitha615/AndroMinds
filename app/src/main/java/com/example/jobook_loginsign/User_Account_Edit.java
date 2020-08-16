e98f66693af6c1122787d0830cecd583e9cfa06b
Sanduni Galagoda <srg19981234@gmail.com>
2020-08-16 19:38:46 +0530

    First Designing

---
+++ app/src/main/java/com/example/jobook_loginsign/User_Account_Edit.java
@@ -0,0 +1,14 @@
+package com.example.jobook_loginsign;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class User_Account_Edit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user__account__edit);
+    }
+}
