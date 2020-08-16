076df3c968ea8565604c72e44bd8882472ac6a92
Nimesha-Gunarathne <nimesha.g98@gmail.com>
2020-08-16 17:59:52 +0530

    Nime Edited string files

---
+++ app/src/main/java/com/example/jobook_loginsign/View_Applicants.java
@@ -0,0 +1,14 @@
+package com.example.jobook_loginsign;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class View_Applicants extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view__applicants);
+    }
+}
