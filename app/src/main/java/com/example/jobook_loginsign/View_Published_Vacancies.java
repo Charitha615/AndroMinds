034f60cd533913d4e0bc88a1042b360a4fbd74b0
ashi1015 <ayeshpramo@gmail.com>
2020-08-16 12:40:43 +0530

    Qualification title Changed made in strings.xml file

---
+++ app/src/main/java/com/example/jobook_loginsign/View_Published_Vacancies.java
@@ -0,0 +1,14 @@
+package com.example.jobook_loginsign;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class View_Published_Vacancies extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view__published__vacancies);
+    }
+}
---
+++ app/src/main/java/com/example/jobook_loginsign/view_vacancy.java
@@ -0,0 +1,14 @@
+package com.example.jobook_loginsign;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class view_vacancy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_vacany);
+    }
+}
